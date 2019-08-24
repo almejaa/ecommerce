94aca5175426e3a77ac3cf9b86d716d9b98d8ae2
Alejandra <54401182+almejaa@users.noreply.github.com>
2019-08-24 00:20:27 -0300

    Primera entrega
    Primera entrega, parte obligatoria completa.

--- js/products.js
+++ js/products.js
@@ -1,14 +1,53 @@
+const ORDER_ASC_BY_NAME = "AZ";
+const ORDER_DESC_BY_NAME = "ZA";
+const ORDER_BY_PROD_COUNT = "Cant.";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
+
 function sortProducts(criteria, array){
 
 }
 
 function showProductsList(array){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
 
+        if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <h4>` + product.cost + " " + product.currency+ `</h4>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <small class="text-muted">` + product.soldCount + ` unidades vendidas`+`</small>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductsArray = resultObj.data;
+            showProductsList(currentProductsArray);
+        }
+    });
 });
