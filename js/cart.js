c639733d73f4c216576d3d8cc6c40690df926906
Alejandra <54401182+almejaa@users.noreply.github.com>
2019-10-19 03:31:28 -0300

    Final parte obligatoria 3

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,5 @@
 let productUnitCost = 0;
+let productCount = 0;
 let productCurrency = "";
 let subtotal = 0;
 let shippingPercentage = 0.15;
@@ -10,11 +11,22 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts() {
+    total = subtotal + subtotal * shippingPercentage;
+    $("#total").text(total+" "+productCurrency);
+}
 
+function updateArticleCount() {
+    productCount = $("#valCant").val();
+    $("#cantidad").text(productCount+" Artículos");
+    updateSubtotal();
 }
 
 function updateSubtotal() {
-
+    subtotal = productUnitCost * productCount;
+    $("#subtotal").text(subtotal+" "+productCurrency);
+    $("#envio").text(subtotal*shippingPercentage+" "+productCurrency);
+    $("#unit").text(subtotal+" "+productCurrency);
+    updateTotalCosts();
 }
 
 function showPaymentTypeNotSelected() {
@@ -31,20 +43,19 @@ function showArticles(articles) {
         let article = articles[i];
 
         htmlContentToAppend += `
-        <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + article.src + `" alt="` + article.count + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ article.name + `</h4>
-                            <h4>` + article.unitCost + " " + article.currency + `</h4>
-                        </div>
-                        <p class="mb-1">` + article.count + `</p>
+        <tr>
+            <th scope="row" class="border-0">
+                <div class="p-2">
+                    <img src="` + article.src + `" alt="" width="70" class="img-fluid rounded shadow-sm">
+                    <div class="ml-3 d-inline-block align-middle">
+                        <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">`+ article.name + `</a></h5>
                     </div>
                 </div>
-            </a>
+            </th>
+            <td class="border-0 align-middle"><strong>` + article.unitCost + " " + article.currency + `</strong></td>
+            <td class="border-0 align-middle"><strong><input id="valCant" onchange="updateArticleCount()" type="number" min="0" value="` + article.count + `"></strong></td>
+            <td class="border-0 align-middle"><strong id = "unit">` + article.unitCost * productCount + " " + article.currency + `</strong></td>
+        </tr>
             `
     }
     document.getElementById("cart-list-container").innerHTML = htmlContentToAppend;
@@ -57,7 +68,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(CART_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             var array = resultObj.data.articles;
+            productUnitCost = array[0].unitCost;
+            productCurrency = array[0].currency;
+            productCount = array[0].count;
             showArticles(array);
+            updateArticleCount();
         }
     });
 });
